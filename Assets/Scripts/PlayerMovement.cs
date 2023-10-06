3bea5ed303472a6fb060a40adf82502013e845e6
Leo Lunev <loganlev@bk.ru>
2023-10-06 13:39:02 +0300

    Feat: Add features to PlayerMovement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,7 @@
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
@@ -7,28 +10,69 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] private LayerMask groundLayer;
 	[SerializeField] private Transform groundChecker;
 	[SerializeField] private float checkGroundRadius = 0.4f;
-	[SerializeField] private float jumpHeight = 4f;
+	[SerializeField] private float jumpHeight = 6f;
 
 	private CharacterController character;
 
+	private float maxSpeed = 8f;
+	private float acceleration = 30f;
+	private float deceleration = 20f;
+	float verticalSpeed = 0;
+	float horizontalSpeed = 0;
+
 	private float verticalVelocity;
-	private float speed = 4;
-	private float gravity = -9.81f;
+	private float gravity = -40f;
 	private bool isGrounded;
+
+	private float jumpBufferTime = 0.1f; //Buffer time is working wrong when the second jump is too soon, it still makes jump
+	private float jumpBufferCounter;
+
+	private float coyoteTime = 0.2f;
+	private float coyoteTimeCounter;
+
+	private bool canMove = true;
 
 	private void OnEnable()
 	{
-		InputManager.OnTouchStarted += Jump;
+		InputManager.OnTouchStarted += HandleJumpBuffer;
+		InputManager.OnTouchEnded += GoDown;
 	}
 
 	private void OnDisable()
 	{
-		InputManager.OnTouchStarted -= Jump;
+		InputManager.OnTouchStarted -= HandleJumpBuffer;
+		InputManager.OnTouchEnded -= GoDown;
 	}
 
 	private void Start()
 	{
 		character = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		if (!canMove)
+		{
+			horizontalSpeed = 0;
+			verticalSpeed = 0;
+			verticalVelocity = 0;
+		}
+		if (isGrounded)
+		{
+			coyoteTimeCounter = coyoteTime;
+		}
+		else
+		{
+			coyoteTimeCounter -= Time.deltaTime;
+		}
+
+		jumpBufferCounter -= Time.deltaTime;
+		Jump();
+
+		if (joystick.Horizontal != 0 ||  joystick.Vertical != 0)
+		{
+			Rotation();
+		}
 	}
 
 	private void FixedUpdate()
@@ -40,16 +84,59 @@ public class PlayerMovement : MonoBehaviour
 
 	private void Move()
 	{
-		var velocity = new Vector3(joystick.Horizontal, 0, joystick.Vertical) * speed;
+		float horizontalSpeedDif = maxSpeed - horizontalSpeed;
+		float verticalSpeedDif = maxSpeed - verticalSpeed;
+		float maxHorSpeed = joystick.Horizontal * maxSpeed;
+		float maxVertSpeed = joystick.Vertical * maxSpeed;
+
+
+		if (joystick.Horizontal != 0)
+		{
+			horizontalSpeed = Mathf.MoveTowards(horizontalSpeed, maxHorSpeed, acceleration*Time.deltaTime);
+		}
+
+		if (joystick.Vertical != 0)
+		{
+			verticalSpeed = Mathf.MoveTowards(verticalSpeed, maxVertSpeed, acceleration * Time.deltaTime);
+		}
+
+		if (joystick.Horizontal == 0)
+		{
+			horizontalSpeed = Mathf.MoveTowards(horizontalSpeed, 0, deceleration * Time.deltaTime);
+		}
+
+		if (joystick.Vertical == 0)
+		{
+			verticalSpeed = Mathf.MoveTowards(verticalSpeed, 0, deceleration * Time.deltaTime);
+		}
+
+		//verticalSpeed = Mathf.Clamp(verticalSpeed, -maxSpeed, maxSpeed);
+		//horizontalSpeed = Mathf.Clamp(horizontalSpeed, -maxSpeed, maxSpeed);
+
+		var velocity = new Vector3(horizontalSpeed, 0, verticalSpeed);
 		character.Move(velocity * Time.fixedDeltaTime);
+	}
+
+	private void Rotation()
+	{
+		var direction = new Vector3 (joystick.Horizontal, 0, joystick.Vertical);
+		Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+		transform.rotation = rotation;
 	}
 
 	public void Jump()
 	{
-		if (isGrounded)
+		if (coyoteTimeCounter > 0 && jumpBufferCounter > 0)
 		{
 			verticalVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
+			coyoteTimeCounter = 0f;
+			jumpBufferCounter = 0f;
 		}
+	}
+
+	private void HandleJumpBuffer()
+	{
+		jumpBufferCounter = jumpBufferTime;
 	}
 
 	private void DoGravity()
@@ -72,5 +159,25 @@ public class PlayerMovement : MonoBehaviour
 		{
 			verticalVelocity = -2;
 		}
+	}
+
+	private void GoDown()
+	{
+		if (verticalVelocity > 0)
+		{
+			verticalVelocity *= 0.2f;
+		}
+	}
+
+	public void Stop()
+	{
+		horizontalSpeed = 0f;
+		verticalSpeed = 0f;
+		verticalVelocity = 0f;
+		canMove = false;
+	}
+	public void ContinueMovement()
+	{
+		canMove = true;
 	}
 }
