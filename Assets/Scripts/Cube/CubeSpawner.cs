e959aca7a76902894d6ecd2b7f35a04e78441f8c
Leo Lunev <loganlev@bk.ru>
2023-10-09 21:21:47 +0300

    Fix: Add prevention of same random X in CubeSpawner.

--- Assets/Scripts/Cube/CubeSpawner.cs
+++ Assets/Scripts/Cube/CubeSpawner.cs
@@ -4,6 +4,7 @@ public class CubeSpawner : MonoBehaviour
 {
 	[SerializeField] private CubeBase cube;
 	private Vector3 lastPosition;
+	private float previousRandomX = 0;
 
 	private void Start()
 	{
@@ -23,7 +24,13 @@ public class CubeSpawner : MonoBehaviour
 	private Vector3 GetRandomOffset()
 	{
 		var randomX = Random.Range(-4,4);
-		var randomY = Random.Range(2,4);
+		while (randomX == previousRandomX)
+		{
+			randomX = Random.Range(-4, 4);
+		}
+		previousRandomX = randomX;
+
+		var randomY = Random.Range(2,5);
 		var randomZ = Random.Range(-4,4);
 		return new Vector3(randomX,randomY,randomZ);
 	}
