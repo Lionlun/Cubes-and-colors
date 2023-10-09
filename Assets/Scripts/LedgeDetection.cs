2c07f1e8afd7972d6d0c74785592ff91f16ad4c5
Leo Lunev <loganlev@bk.ru>
2023-10-09 21:18:41 +0300

    Fix: Change LedgeDetection, LedgeClimb.

--- Assets/Scripts/LedgeDetection.cs
+++ Assets/Scripts/LedgeDetection.cs
@@ -2,67 +2,123 @@ using UnityEngine;
 
 public class LedgeDetection : MonoBehaviour
 {
-	public bool IsDetected { get; set; }
-	public Vector3 Position { get; set; }
+	private bool onWall;
+	private bool onWallUp;
+	private bool onWallDown;
+	private bool onLedge;
+	private bool isAlreadyMoved;
 
-	[SerializeField] private float radius;
-	[SerializeField] private LayerMask mask;
+	[SerializeField] private float wallCheckRayDistance = 1f;
+	[SerializeField] private float ledgeRayCorrectY = 0.5f;
+	[SerializeField] private float offsetY;
+	private float minCorrectionY = 0.01f;
 
-	private bool canDetect = true;
-	private float detectTime = 0.1f;
-	private float detectTimer;
+	[SerializeField] private Transform wallCheckUp;
+	[SerializeField] private Transform wallCheckDown;
+	[SerializeField] private PlayerMovement player;
+	[SerializeField] private LayerMask groundLayer;
+	[SerializeField] private Animator animator;
+
+	private void OnEnable()
+	{
+		InputManager.OnTouchStarted += Climb;
+	}
+
+	private void OnDisable()
+	{
+		InputManager.OnTouchStarted -= Climb;
+	}
 
 
 	private void Update()
 	{
-		if (canDetect)
+		
+	}
+
+	private void FixedUpdate()
+	{
+		CheckLedge();
+	}
+
+	private void Climb()
+	{
+		if (onLedge)
 		{
-			DetectLedge();
-		}
-		HandleDetectTimer();
-		if (detectTimer < 0) 
-		{
-			IsDetected = false;
+			animator.SetTrigger("Climb");
 		}
 	}
 
-	private  void DetectLedge()
+	private void CheckLedge()
 	{
-		Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius, mask);
-		foreach (Collider collider in hitColliders)
+		onWallUp = Physics.Raycast(wallCheckUp.position, transform.forward, wallCheckRayDistance, groundLayer);
+
+		if (onWallUp)
 		{
-			if (collider.gameObject.GetComponent<CubeBase>() != null)
+			onLedge = !Physics.Raycast(wallCheckUp.position + new Vector3(0, ledgeRayCorrectY, 0), transform.forward, wallCheckRayDistance, groundLayer);
+			MoveTowardsLedge();
+		}
+		else
+		{
+			onLedge = false;
+		}
+
+		animator.SetBool("OnLedge", onLedge);
+
+		if (onLedge)
+		{	
+			player.Stop();
+			CalculateCorrectOffset();
+		
+		}
+	}
+
+	private void MoveTowardsLedge()
+	{
+		if (isAlreadyMoved)
+		{
+			return;
+		}
+		isAlreadyMoved = true;
+		Ray ray = new Ray(wallCheckUp.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, wallCheckRayDistance, groundLayer))
+		{
+			var direction = hit.point - player.transform.position;
+			player.transform.position += new Vector3(direction.x, 0, direction.z).normalized * hit.distance/1.5f;
+		}
+	}
+
+	private  void CalculateCorrectOffset()
+	{
+		Ray ray = new Ray(wallCheckUp.position + new Vector3(0, ledgeRayCorrectY, 0) + transform.forward * wallCheckRayDistance, Vector3.down);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, ledgeRayCorrectY, groundLayer))
+		{
+			offsetY = hit.distance;
+
+			if(offsetY > minCorrectionY * 1.5f)
 			{
-				detectTimer = detectTime;
-				IsDetected = true;
-				Position = collider.transform.position;
+				player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - offsetY + minCorrectionY, player.transform.position.z);
+				isAlreadyMoved = false;
 			}
 		}
 	}
 
-	private void HandleDetectTimer()
-	{
-		detectTimer -= Time.deltaTime;
-	}
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.GetComponent<CubeBase>() != null)
-		{
-			canDetect = false;
-		}
-	}
-
-	private void OnTriggerExit(Collider other)
-	{
-		if (other.gameObject.GetComponent<CubeBase>() != null)
-		{
-			canDetect = true;
-		}
-	}
 
 	private void OnDrawGizmos()
 	{
-		Gizmos.DrawWireSphere(transform.position, radius);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawLine(wallCheckUp.position, wallCheckUp.position + wallCheckUp.forward * wallCheckRayDistance);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(wallCheckUp.position + new Vector3(0, ledgeRayCorrectY, 0),
+			wallCheckUp.position + new Vector3(0, ledgeRayCorrectY, 0) + wallCheckUp.forward * wallCheckRayDistance);
+		
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine(wallCheckUp.position + new Vector3(0, ledgeRayCorrectY, 0) + transform.forward * wallCheckRayDistance,
+			wallCheckUp.position + new Vector3(0, ledgeRayCorrectY, 0) + transform.forward * wallCheckRayDistance + Vector3.down * ledgeRayCorrectY);
 	}
 }
--- Assets/Scripts/Player/LedgeClimb.cs
+++ Assets/Scripts/Player/LedgeClimb.cs
@@ -1,55 +1,7 @@
-using System.Threading.Tasks;
+
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerMovement))]
 public class LedgeClimb : MonoBehaviour
 {
-	[SerializeField] private LedgeDetection ledgeDetection;
-	private Vector3 offset1 = new Vector3 (0, 1, 1);
-	private Vector3 offset2 = new Vector3 (0, 1, 1);
-	private Vector3 climbStartPosition;
-	private Vector3 climbEndPosition;
-	private bool canClimb;
-	private bool canGrabLedge = true;
-	private PlayerMovement playerMovement;
-
-	private void Start()
-	{
-		playerMovement = GetComponent<PlayerMovement>();
-	}
-
-	private void Update()
-	{
-		Climb();
-	}
-
-	private void Climb()
-	{
-		if (ledgeDetection.IsDetected && canGrabLedge)
-		{
-			canGrabLedge = false;
-			Vector2 ledgePosition = ledgeDetection.transform.position;
-			climbStartPosition = ledgeDetection.transform.position + offset1;
-			climbEndPosition = ledgeDetection.transform.position + offset2;
-			canClimb = true;
-			
-		}
-
-		if (canClimb)
-		{
-			transform.position = climbStartPosition;
-			EndClimb();
-		}
-	}
-
-	private async void EndClimb()
-	{
-		canClimb = false;
-		playerMovement.Stop();
-		var moveDirection = ledgeDetection.Position + new Vector3(0, 0.5f, 0);
-		transform.position = moveDirection;
-		await Task.Delay(500);
-		canGrabLedge = true;
-		playerMovement.ContinueMovement();
-	}
+	
 }
